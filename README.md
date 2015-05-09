1ca0bdc29184f3e64595380ce5ff07e33eb65286
mewfree <damien.gonot@gmail.com>
2015-05-09 14:57:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# flowdock-wikipedia-bot
+The bot searches Wikipedia articles and display the first hundreds of characters in Flowdock.
+
+Example: "!wikipedia Canada" should display the beginning of the Wikipedia article about Canada.
+
+Requires a conf.json file with a flowdock API key ("fdkey").
---
+++ main.js
@@ -0,0 +1,33 @@
+var conf = require('./conf.json');
+
+var Wiki = require('wikijs');
+var wiki = new Wiki();
+
+var Session = require('flowdock').Session;
+var session = new Session(conf.fdkey);
+
+var CODE = '!wikipedia';
+
+session.flows(function(err, flows) {
+  var anotherStream, flowIds;
+  flowIds = flows.map(function(f) {
+    return f.id;
+  });
+  anotherStream = session.stream(flowIds);
+  return anotherStream.on('message', function(message) {
+    if (message.event == 'message') {
+      var query = message.content;
+      query = query.split(' ');
+      if (query[0] == CODE) {
+        query.shift();
+        query = query.toString().replace(/,/g, ' ');
+        wiki.page(query).then(function(page) {
+          page.content().then(function(content) {
+            var output = content.substring(0,400)+'...';
+            session.comment(message.flow, message.id, output);
+          });
+        });
+      }
+    }
+  });
+});
